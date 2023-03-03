97620c056aa49bbe0e1c82b21c6dd2e42330ceeb
unknown <xiaobingpang@qq.com>
2023-03-03 13:29:30 +0800

    append dll path into PATH variable

--- crates/examples/build.rs
+++ crates/examples/build.rs
@@ -1,3 +1,16 @@
+use std::env;
+
 fn main() {
-    println!("cargo:rustc-env=LD_LIBRARY_PATH=crates/ctp-futures/v_current");
+    if cfg!(windows) {
+        let key = "PATH";
+        match env::var(key) {
+            Ok(val) => {
+                let path = format!("{val};crates\\ctp-futures\\v_current");
+                println!("cargo:rustc-env=PATH={path}");
+            }
+            Err(e) => println!("couldn't interpret {key}: {e}"),
+        }
+    } else {
+        println!("cargo:rustc-env=LD_LIBRARY_PATH=crates/ctp-futures/v_current");
+    }
 }
